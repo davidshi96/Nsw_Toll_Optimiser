4c78d96659fb8b50d6a4b86a5aa1445034b176d8
davidshi96 <davidshi1996@live.com>
2021-01-31 12:08:00 +1100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+class Map extends React.Component {
+    
+    
+    componentDidMount() {
+
+    }
+
+    render() {
+      return null;
+    }
+  }
